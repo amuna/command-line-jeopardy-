42c041fe3539905ab616636b85b3cdbff9f6f0ad
zainansari0417 <muhammad.ansari@uoit.net>
2018-02-26 21:38:58 -0500

    Update players.h

--- players.h
+++ players.h
@@ -1,9 +1,5 @@
 /*
  * Tutorial 4 Jeopardy Project for SOFE 3950U / CSCI 3020U: Operating Systems
- *
- * Copyright (C) 2015, <GROUP MEMBERS>
- * All rights reserved.
- *
  */
 #ifndef PLAYERS_H_
 #define PLAYERS_H_
@@ -25,4 +21,4 @@ extern bool player_exists(struct player *players, int num_players, char *name);
 // player given their name
 extern void update_score(struct player *players, int num_players, char *name, int score);
 
-#endif /* PLAYERS_H_ */
+#endif
